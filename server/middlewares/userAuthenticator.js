69fbdbfbed6467b9b35f21ac40a6adbe52f73af2
Nityagopaljena98 <nityagjena@hotmail.com>
2025-05-13 02:17:23 +0530

    update the logic to auto logout when token expire

--- server/middlewares/userAuthenticator.js
+++ server/middlewares/userAuthenticator.js
@@ -1,7 +1,8 @@
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
 
 const authenticateUser = (req, res, next) => {
     const token = req.headers.authorization?.split(" ")[1];
+    
     if (!token) {
         return res.status(401).json({ message: "Access denied. No token provided." });
     }
@@ -10,10 +11,13 @@ const authenticateUser = (req, res, next) => {
         const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decodedToken;
         next();
-
     } catch (error) {
-        res.status(401).json({ message: "Invalid or expired token." });
+        if (error.name === 'TokenExpiredError') {
+            return res.status(401).json({ message: "Token expired. Please login again." });
+        } else {
+            return res.status(401).json({ message: "Invalid token." });
+        }
     }
-}
+};
 
-export default authenticateUser
+export default authenticateUser;
